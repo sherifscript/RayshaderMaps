b2a54cbc0e0a9e1daaa9369d4e23d3f38ddbe875
sherifscript <hyperachilles@gmail.com>
2023-06-20 23:20:51 +0200

    First commit

---
+++ Germany/main.R
@@ -0,0 +1,94 @@
+# Load Required Libraries
+library(sf)
+library(tidyverse)
+library(stars)
+library(rayshader)
+library(MetBrewer)
+library(colorspace)
+
+# Load Kontur Data
+data <- st_read("kontur_population_DE_20220630.gpkg")
+
+# Set Bounding Box
+bb <- st_bbox(data)
+
+# Determine Width and Height
+## Calculate width
+bottom_left <- st_point(c(bb[['xmin']], bb[['ymin']])) |> 
+  st_sfc(crs=st_crs(data))
+
+bottom_right <- st_point(c(bb[['xmax']], bb[['ymin']])) |> 
+  st_sfc(crs=st_crs(data))
+
+width <- st_distance(bottom_left, bottom_right)
+
+## Calculate height
+top_left <- st_point(c(bb[['xmin']], bb[['ymax']])) |> 
+  st_sfc(crs=st_crs(data))
+
+height <- st_distance(top_left, bottom_left)
+
+# Handle Aspect Ratio
+if (width > height) {
+  w_ratio <- 1
+  h_ratio <- height / width
+} else {
+  h_ratio <- 1
+  w_ratio <- width / height
+}
+
+# Data Conversion: Spatial Data to Matrix
+size = 6000
+rast <- st_rasterize(data, nx=floor(size*w_ratio), 
+                           ny=floor(size*h_ratio))
+mat <- matrix(data=rast$population, 
+              nrow =floor(size*w_ratio), 
+              ncol =floor(size*h_ratio))
+
+# Generate Color Palette using MetBrewer
+c1 <- met.brewer('Benedictus')
+swatchplot(c1)
+texture <- grDevices::colorRampPalette(c1, bias=2)(256)
+swatchplot(texture)
+
+# Create 3D Plot using Rayshader
+# (If the plot_3d function causes a fatal error,install Rayshader
+#  directly from Github using the remotes package.)
+
+rgl::close3d() # Close any open RGL windows
+mat |> height_shade(texture = texture) |> 
+  plot_3d(heightmap = mat, zscale = 200/6, 
+          solid = F, shadowdepth = 0)
+
+# Adjust Camera Angles
+render_camera(theta = -20, phi = 45, zoom = 0.8)
+
+# Render Plot and Save as PNG
+# The approach below ensures that if the render fails but 
+# a file is generated, it can be assumed that the errors weren't
+# associated with incorrect files paths or permissions.
+
+outfile = 'images\\final_plot.png'
+{
+  start_time <- Sys.time()
+  cat(crayon::red(start_time), '\n')
+  if(!file.exists(outfile)){  
+    png::writePNG(matrix(1), target = outfile) 
+  }
+  
+  render_highquality(
+    filename = outfile,
+    interactive = F,
+    lightdirection = 270,
+    lightaltitude = c(50,80),
+    lightcolor = c(c1[6],'white'),
+    lightintensity = c(600,100),
+    samples = 450,
+    width = 6000,
+    height = 6000,
+  )
+  
+  end_time <- Sys.time()
+  diff <- end_time - start_time
+  cat(crayon::red(diff), "\n")
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+# RayshaderMaps
+
+This repository contains my projects for creating 3D population density maps using the [`rayshader`](https://www.rayshader.com/) package in R. The data is sourced from the [Kontur Population Dataset 2022](https://data.humdata.org/dataset/kontur-population-dataset), which estimates worldwide population in 400m hexagonal geometries using a combination of various sources.
+
+The maps are presented at an angle to better illustrate heights, and the texts and labels are added using Adobe Illustrator.
+
+## Projects
+
+### **[Egypt](./Egypt)**
+![Egypt](./Egypt/Egypt Population Density w Rayshader 72dpi-01.png)
+
+### **[Germany](./Germany)**
+![Germany](./Germany/Germany Population Density w Rayshader 72dpi-01.png)
+
+
+### Portraits were created on 19.06.2023 using data from [Kontur's 2022 Global Population Density for 400m H3 Hexagons](https://data.humdata.org/dataset/kontur-population-dataset).
+
+I appreciate any feedback as I continue to learn and explore data visualization, especially regarding aesthetics. 
+
+Check out my other visualizations on my [Reddit account](https://www.reddit.com/user/sherifscript/).
